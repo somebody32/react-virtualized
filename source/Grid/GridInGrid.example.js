57dd9ddd02dbdc96ace30ac58146dc01e1864e81
Ilya Zayats <somebody32@gmail.com>
2016-03-07 18:48:33 +0100

    another example: grid is a sibling of another grid + scroll sync on top through the parent

--- source/Grid/GridInGrid.example.js
+++ source/Grid/GridInGrid.example.js
@@ -2,6 +2,7 @@ import React, { Component, PropTypes } from 'react';
 import { ContentBox, ContentBoxHeader, ContentBoxParagraph } from '../demo/ContentBox'
 import AutoSizer from '../AutoSizer';
 import Grid from './Grid';
+import ScrollSync from '../ScrollSync/';
 
 import styles from './GridInGrid.example.css';
 
@@ -11,6 +12,7 @@ const COLS = 100;
 const grid_width = COL_WIDTH * COLS;
 const ROWS_NUMBER = 100;
 const rows = Array.apply(null, {length: ROWS_NUMBER}).map(Function.call, n =>  n * 20);
+const grid_height = ROWS_NUMBER * ROW_HEIGHT;
 
 const GRID_BG_COLOR = 'rgb(255, 255, 255)';
 const GRID_WEEKEND_BG_COLOR = 'rgb(246, 246, 246)';
@@ -25,6 +27,7 @@ export default class GridInGridExample extends Component {
     super()
     this.renderCol = this.renderCol.bind(this);
     this.renderRow = this.renderRow.bind(this);
+    this.position = {gridLeft: 0, gridTop: 0};
   }
 
   renderCol({columnIndex}) {
@@ -35,36 +38,65 @@ export default class GridInGridExample extends Component {
   renderRow({rowIndex}) {
     const transform = `translatex(${rows[rowIndex]}px)`;
     const width = rows[rowIndex];
-    return <div className={styles.row} style={{transform, width}} />
+    return <div onClick={() => console.log('clicked')} className={styles.row} style={{transform, width}} />
   }
 
   render() {
     return (
       <div className={styles.wrapper}>
-        <AutoSizer>
-          {({ height, width }) => (
-            <Grid
-              rowsCount={1}
-              width={width}
-              height={height}
-              columnWidth={COL_WIDTH}
-              columnsCount={COLS}
-              rowHeight={height}
-              renderCell={this.renderCol}
-            >
-            <Grid
-              style={{position: 'absolute', top: 0}}
-              rowsCount={rows.length}
-              width={grid_width}
-              height={height}
-              overscanRowsCount={15}
-              columnWidth={grid_width - 50}
-              columnsCount={1}
-              rowHeight={ROW_HEIGHT}
-              renderCell={this.renderRow} />
-            </Grid>
+        <ScrollSync>
+          {({ onScroll, scrollLeft, scrollTop }) => (
+            <AutoSizer>
+              {({ height, width }) => (
+                <div
+                  ref={(node) => this.wrapper = node}
+                  style={{height, width}}
+                  className={styles.grid_wrapper}
+                  onScroll={
+                    (event) => {
+                      if (this.wrapper !== event.target) return;
+                      onScroll({ scrollLeft: event.target.scrollLeft, scrollTop: event.target.scrollTop })
+                      this.position = {gridTop: event.target.scrollTop, gridLeft: event.target.scrollLeft};
+                    }
+                  }
+                >
+                  <div
+                    style={{height: grid_height, width: grid_width}}
+                    className={styles.scroller}
+                    >
+                    <div
+                      style={{top: this.position.gridTop, left: this.position.gridLeft}}
+                      className={styles.innerWrapper}
+                    >
+                      <Grid
+                        className={styles.gridStyle}
+                        rowsCount={1}
+                        width={width}
+                        height={height}
+                        columnWidth={COL_WIDTH}
+                        columnsCount={COLS}
+                        rowHeight={height}
+                        renderCell={this.renderCol}
+                        scrollLeft={scrollLeft}
+                        scrollTop={scrollTop}/>
+                      <Grid
+                        className={styles.gridStyle}
+                        rowsCount={rows.length}
+                        width={width}
+                        height={height}
+                        columnWidth={grid_width}
+                        columnsCount={1}
+                        rowHeight={ROW_HEIGHT}
+                        renderCell={this.renderRow}
+                        scrollLeft={scrollLeft}
+                        scrollTop={scrollTop}/>
+                    </div>
+                  </div>
+                </div>
+              )}
+            </AutoSizer>
           )}
-        </AutoSizer>
+        </ScrollSync>
       </div>
     );
   }
